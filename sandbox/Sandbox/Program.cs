b410f73fc1154d613940e6362af62cbedac78c11
legendairee <augustinelegend@gmail.com>
2026-08-31 00:02:05 +0100

    Finished week01 exercise1

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox Project.");
+        Console.WriteLine("Hello, this is mathew Udowa testing thing up");
+
+        Console.Write("What is your favorite color? ");
+        string color = Console.ReadLine();
+        Console.WriteLine($"{color} Is my favorite color.");
     }
 }
