d5c426a0d43e0d132bd5a681b8255a2a7f793e73
legendairee <augustinelegend@gmail.com>
2026-08-31 00:09:57 +0100

    Week01 assignment exercise1 completed

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -9,6 +9,5 @@ class Program
         Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
-        
     }
 }
