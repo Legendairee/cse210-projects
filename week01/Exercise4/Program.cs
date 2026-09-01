a70fe99dd2a3658b69f73c1687e3778ed524ee09
legendairee <augustinelegend@gmail.com>
2026-09-01 04:35:08 +0100

    Week01 assignment exercise4 completed

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,73 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int userNumber = 1;
+
+        while (userNumber != 0)
+        {
+            Console.Write("Please enter a number (type 0 when finished): ");
+            userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+
+        }
+
+        if (numbers.Count > 0)
+        {
+            int sum = 0;
+
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+
+            double average = (double)sum / numbers.Count;
+
+            int largest = numbers[0];
+
+            foreach (int number in numbers)
+            {
+                if (number > largest)
+                {
+                    largest = number;
+                }
+            }
+
+            int smallestPositive = 999999999;
+
+            foreach (int number in numbers)
+            {
+                if (number > 0 && number < smallestPositive)
+                {
+                    smallestPositive = number;
+                }
+            }
+            Console.WriteLine($"The sum is: {sum}");
+            Console.WriteLine($"The average is: {average}");
+            Console.WriteLine($"The largest number is: {largest}");
+            Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+
+            
+            numbers.Sort();
+            Console.WriteLine("The sorted list is: ");
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+        else
+        {
+            Console.WriteLine("No numbers were entered.");
+        }
+
+
     }
 }
