5c6141b9992e4de1c4fa0b050e685568bfffdd45
legendairee <augustinelegend@gmail.com>
2026-09-01 01:12:28 +0100

    Week01 assignment exercise3 completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string keepPlaying = "yes";
+
+        while (keepPlaying == "yes")
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 101);
+            int guessCount = 0;
+            int guess;
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (guess > number)
+                {
+                    Console.WriteLine("Guess Lower");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Guess Higher");
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("===========================================");
+                    Console.WriteLine("Congratulations! You guessed right.");
+                }
+
+            } while (guess != number);
+
+            Console.WriteLine($"It took you {guessCount} guesses");
+            Console.Write("Would you like to play again (yes/no)? ");
+            keepPlaying = Console.ReadLine().ToLower();
+            Console.WriteLine("===========================================");
+            Console.WriteLine("");
+        }
+
+        Console.WriteLine("Thank you for playing. Goodbye");
+        Console.WriteLine("====================================");
     }
 }
