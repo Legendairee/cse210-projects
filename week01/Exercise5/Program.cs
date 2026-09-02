923532d09646a5db88195f6dc1211c3ccbea7728
legendairee <augustinelegend@gmail.com>
2026-09-02 23:47:58 +0100

    Moved my function outside main

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,47 +2,46 @@ using System;
 
 class Program
 {
+   
     static void Main(string[] args)
     {
-
-        static void DisplayWelcome()
-        {
-            Console.WriteLine("Welcome to the program!");
-        }
-        
         DisplayWelcome();
 
-        static string PromptUserName()
-        {
-            Console.Write("Please enter your name: ");
-            string name = Console.ReadLine();
-            return name;
-        }
         string UserName = PromptUserName();
 
-
-        static int PromptUserNumber()
-        {
-            Console.Write("Please enter your favorite number: ");
-            int number = int.Parse(Console.ReadLine());
-            return number;
-        }
         int MyFavoriteNumber = PromptUserNumber();
 
-
-        static int SquareNumber(int number)
-        {
-            return number * number;
-        }
         int MySquareNumber = SquareNumber(MyFavoriteNumber);
 
-
-        static void DisplayResult(string name, int number)
-        {
-            Console.WriteLine($"{name}, the square of your number is {number}");
-        }
-
         DisplayResult(UserName, MySquareNumber);
+    }
 
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(string name, int number)
+    {
+        Console.WriteLine($"{name}, the square of your number is {number}");
     }
 }
