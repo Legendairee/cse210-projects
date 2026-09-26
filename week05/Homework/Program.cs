2ad9a35b10f90c9a95b00c812bb6e6eb5d908f01
legendairee <augustinelegend@gmail.com>
2026-09-26 16:43:12 +0100

    Completed homework assignment on inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine("==================================");
+        Console.WriteLine();
+        Console.WriteLine("========= Math Assignment ========");
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodrigruez", "Fractions", "7.3", "8-19");
+
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine("==================================");
+        Console.WriteLine();
+        Console.WriteLine("======= Writing Assignment =======");
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+        Console.WriteLine();
     }
 }
